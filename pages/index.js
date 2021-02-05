d090d99b0d3ce0199372f0d1b65277ebbf7e6c6a
yudete <yu@yude.moe>
2021-02-06 02:31:04 +0900

    Setup Next.js

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import Layout from "../components/Layout"
+import Link from 'next/link'
+import Image from 'next/image'
+
+const Index = () => (
+
+    <Layout title="ホーム">
+      <div>
+      <Image className="avatar"
+        src    = "/static/images/avatar.png"
+        alt    = "yude's avatar"
+        width  = {200}
+        height = {200}
+        />
+        <p>🔨 This page is under the construction. 🔨</p>
+        <p>For now, please visit <Link href="https://yude.moe"><a>yude.moe</a></Link>.
+        </p>
+      </div>
+    </Layout>
+    
+  )
+  
+export default Index
