a38e8d158b813df5135c501b83abad2401da4e79
yudete <yu@yude.moe>
2021-02-07 11:28:57 +0900

    Add packages, Fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  future: {
+    purgeLayersByDefault: true,
+  },
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: "class", // or 'media' or 'class'
   theme: {
