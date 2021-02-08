cb3ce63e78d600855dc05762f1124c219ab71985
yudete <yu@yude.moe>
2021-02-08 17:01:20 +0900

    Remove border

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -33,7 +33,7 @@ const Layout = (props) => {
         
       `}</style>
       <div className="container mx-auto px-6">
-        <div className="mt-16 border-t-2 border-gray-300 flex flex-col items-center">
+        <div className="mt-16 flex flex-col items-center">
             <div className="sm:w-full text-center py-6">
                 <p className="text-sm font-bold mb-2 text-gray-400">
                     {footer} / <Link href="https://github.com/yudemoe/yude.jp"><a>{source}</a></Link>
