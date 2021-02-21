954a166d2e52073a9ad1428b217dfc3dd9c71738
yudete <yu@yude.moe>
2021-02-21 16:54:28 +0900

    Add animation, allow animate-* to use hover: and focus:

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,9 +25,19 @@ module.exports = {
     extend: {
       animation:{
         blur: 'blur 2.5s linear infinite',
-        heartbeat: 'heartbeat 3s linear infinite'
+        heartbeat: 'heartbeat 3s linear infinite',
+        rumble: 'rumble 0.1s linear infinite',
       },
       keyframes:{
+        rumble: {
+          '0%': { transform: 'translate(0, 0)', filter: 'rotate(0)' },
+          '16.66%': { transform: 'translate(4px, -2px)', filter: 'rotate(-5deg)' },
+          '33.32%': { transform: 'translate(4px, 2px)', filter: 'rotate(5deg)' },
+          '49.98%': { transform: 'translate(0, 0)', filter: 'rotate(0)' },
+          '66.64%': { transform: 'translate(-4px, -2px)', filter: 'rotate(5deg)' },
+          '83.30%': { transform:  'translate(-4px, 2px)', filter: 'rotate(-5deg)' },
+          '100%': { transform:  'translate(0, 0)', filter: 'rotate(0)' },
+      },
         blur: {
           '0%, 100%': {filter: 'blur(8px)'},
           '50%': {filter: 'blur(0px)'}
@@ -36,10 +46,11 @@ module.exports = {
           '0%, 100%': {filter: 'drop-shadow(0px 0px 1px  rgb(255, 255, 255))'},
           '50%': {filter: 'drop-shadow(0px 0px 8px  rgb(255, 255, 255))'}
         },
-    }
+    },
     }
   },
   variants: {
+    animation: ['hover', 'focus'], // allow animate-* to use hover: and focus:
     filter: ['responsive'], // defaults to ['responsive']
     backdropFilter: ['responsive'], // defaults to ['responsive']
     aspectRatio: ['responsive'],
