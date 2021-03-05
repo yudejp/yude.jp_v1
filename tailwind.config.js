554cce901ef0f568736ad7e31ccdbb20ea24aee6
yudejp <i@yude.jp>
2021-03-06 01:30:12 +0900

    Change darkMode option to "media"

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     purgeLayersByDefault: true,
   },
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: "class", // or 'media' or 'class'
+  darkMode: "media", // or 'media' or 'class'
   theme: {
     aspectRatio: {
       none: 0,
