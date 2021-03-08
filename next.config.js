3066f5c51ac31444e4b28426ee2b9bf15db65ac3
yudete <yu@yude.moe>
2021-03-08 13:40:37 +0900

    Update locale ids, Update my grade

--- next.config.js
+++ next.config.js
@@ -7,9 +7,9 @@ module.exports = nextTranslate({
     i18n: {
       // These are all the locales you want to support in
       // your application
-      locales: ['ja-JP', 'en-US'],
+      locales: ['ja', 'en'],
       // This is the default locale you want to be used when visiting
       // a non-locale prefixed path e.g. `/hello`
-      defaultLocale: 'ja-JP',
+      defaultLocale: 'ja',
     }
   })
