21d696a39182da6b877a7ac8335548f178bce192
yudejp <i@yude.jp>
2021-05-23 16:27:51 +0900

    Update link for ToS, GitHub Orgs

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -16,10 +16,10 @@ export default function Footer(props) {
             <div className="mt-16 flex flex-col items-center">
                 <div className="sm:w-full text-center py-6">
                     <p className="text-sm font-bold mb-2">
-                        {footer} / <Link href="https://github.com/yudejp/yude.  jp"><a className="hover:underline">{source}</a></Link>
+                        {footer} / <Link href="https://github.com/yudejp/yude.jp"><a className="hover:underline">{source}</a></Link>
                     </p>
                     <p className="text-sm font-bold mb-2">
-                        <Link href="https://scrapbox.io/yude/yude.  jp_%E3%82%B5%E3%83%BC%E3%83%93%E3%82%B9%E5%88%A9%E7%94%A8%E8  %A6%8F%E7%B4%84"><a className="hover:underline">{tos}</a></Link>
+                        <Link href="/tos"><a className="hover:underline">{tos}</a></Link>
                     </p>
                 </div>
               </div>
