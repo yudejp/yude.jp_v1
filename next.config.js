396e7a0af04068f8a8b931683fc61a0da919e585
yudejp <i@yude.jp>
2021-05-23 15:45:54 +0900

    Update configuration for raw-loader

--- next.config.js
+++ next.config.js
@@ -8,4 +8,11 @@ module.exports = nextTranslate({
     images: {
       domains: ['mackerel.io'],
     },
+    webpack: function (config) {
+      config.module.rules.push({
+        test: /\.md$/,
+        use: 'raw-loader',
+      })
+      return config
+    },
   })
