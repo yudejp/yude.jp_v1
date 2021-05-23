20582570fb2b0b05acedfd65b7b7ef92dcac68a4
yudejp <i@yude.jp>
2021-05-23 10:38:58 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 const nextTranslate = require('next-translate')
 
 module.exports = nextTranslate({
-  experimental: {
-    darkModeVariant: true
-  },
     i18n: {
-      // These are all the locales you want to support in
-      // your application
       locales: ['ja', 'en'],
-      // This is the default locale you want to be used when visiting
-      // a non-locale prefixed path e.g. `/hello`
       defaultLocale: 'ja',
     },
     images: {
