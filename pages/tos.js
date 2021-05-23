540e012a11f9d10bb4813b230241665563257616
yudejp <i@yude.jp>
2021-05-23 16:02:08 +0900

    Setup ToS page

---
+++ pages/tos.js
@@ -0,0 +1,21 @@
+import Layout from "./components/Layout"
+import useTranslation from 'next-translate/useTranslation'
+import { useRouter } from 'next/router'
+import React from "react"
+import ReactMarkdown from "react-markdown"
+import gfm from 'remark-gfm';
+import input from '../docs/test.md'
+
+export default function Tos(props) {
+    const router = useRouter()
+    const { locale, locales, defaultLocale, pathname } = router
+    const { t, lang } = useTranslation("tos")
+    const tos = t('tos')
+    return(
+        <Layout title={tos}>
+        <div className="mx-9">
+            <ReactMarkdown plugins={[gfm]} children={input} />
+        </div>
+        </Layout>
+        )
+    }
