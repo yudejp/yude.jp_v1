32064d4ba21c3939829b7cfc80ae6f60349aba8f
yudejp <i@yude.jp>
2021-05-28 11:26:28 +0900

    Try to use getServerSideProps(), Cleanup

--- pages/components/DiscordPlaying.js
+++ pages/components/DiscordPlaying.js
@@ -1,29 +1,35 @@
 import React from "react";
 import useTranslation from 'next-translate/useTranslation'
 import { useRouter } from 'next/router'
-import useRequest from '../lib/useRequest'
+const url = 'https://discord.com/api/guilds/723409709306216498/widget.json';
 
-const App = () => {
+function App ({data}){
   const router = useRouter()
   const { locale, locales, defaultLocale, pathname } = router
   const { t, lang } = useTranslation("common")
-  
-  const { data } = useRequest({
-    url: 'https://discord.com/api/guilds/723409709306216498/widget.json'
-  })
   
   if (data === undefined){
     console.log("Discord API: データの取得に失敗しました。 / Failed to retrieve data.")
     return <p></p>
   }else{
   const str = JSON.stringify(data)
-  const yes_playing = t('yes_playing', {playing: data.members[0].game.name})
+  
   if (str.indexOf("game") !== -1){
+  const yes_playing = t('yes_playing', {playing: data.members[0].game.name})
   return <p>{yes_playing}</p>
   }else{
   return <p></p>
   }
 }
 };
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(url)
+  const data = await res.json()
+
+  // Pass data to the page via props
+  return { props: { data } }
+}
 
 export default App;
--- pages/components/Spotify.js
+++ pages/components/Spotify.js
@@ -1,18 +1,13 @@
 import React from "react";
 import useTranslation from 'next-translate/useTranslation'
 import { useRouter } from 'next/router'
-import useRequest from '../lib/useRequest'
+const url = '/api/Spotify';
 
-const App = () => {
+function App ({data}) {
   const router = useRouter()
   const { locale, locales, defaultLocale, pathname } = router
-  const { t, lang } = useTranslation("common")
-  
-  const [isPlaying, setIsPlaying] = React.useState(0);
-  const { data } = useRequest({
-    url: '/api/Spotify'
-  })
-  
+  const { t, lang } = useTranslation("common")  
+
   if (data === undefined){
     console.log("Spotify Web API: データの取得に失敗しました。 / Failed to retrieve data.")
     return <p></p>
@@ -26,5 +21,14 @@ const App = () => {
   }
 };
 }
+
+export async function getServerSideProps() {
+    // Fetch data from external API
+    const res = await fetch(url)
+    const data = await res.json()
+  
+    // Pass data to the page via props
+    return { props: { data } }
+  }
 
 export default App;
