1bd8eab4911b85e997b638b60fca4b3cb69cad61
yudejp <i@yude.jp>
2021-06-22 10:29:22 +0900

    Fix styling

--- pages/components/LangSelector.js
+++ pages/components/LangSelector.js
@@ -31,7 +31,7 @@ const Dropdown = ({ color }) => {
     <>
 
 <div className="text-left mr-2 ml-2 my-3 float-right">
-    <button type="button" className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" id="options-menu" aria-haspopup="true" aria-expanded="true"
+    <button type="button" className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" id="options-menu" aria-haspopup="true" aria-expanded="true"
     style={{ transition: "all .15s ease" }}
     ref={btnDropdownRef}
     onClick={() => {
@@ -40,8 +40,8 @@ const Dropdown = ({ color }) => {
         : openDropdownPopover();
     }}
     >
-      <FontAwesomeIcon icon={faLanguage} className="w-12 h-6" />
-      <svg className="-mr-1 ml-2 h-5 w-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+      <FontAwesomeIcon icon={faLanguage} className="w-12 h-9" />
+      <svg className="-mr-1 ml-2 h-9 w-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
         <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
       </svg>
     </button>  
