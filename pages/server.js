0d21be23a1eef916a4b167b4572a62138a65888e
yudejp <i@yude.jp>
2021-06-26 22:19:43 +0900

    Fix file name

--- pages/server.js
+++ pages/server.js
@@ -3,7 +3,7 @@ import useTranslation from 'next-translate/useTranslation'
 
 import { useRouter } from 'next/router'
 
-export default function About(props) {
+export default function Server(props) {
   const router = useRouter()
   const { locale, locales, defaultLocale, pathname } = router
   const { t, lang } = useTranslation("server")
