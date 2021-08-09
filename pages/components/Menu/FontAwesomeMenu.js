7f2411b2f6c3b724d9bc9fc6c6cddcc3a0ac3374
yudejp <i@yude.jp>
2021-08-09 11:43:30 +0900

    Try to fix CI

--- pages/components/Menu/FontAwesomeMenu.js
+++ pages/components/Menu/FontAwesomeMenu.js
@@ -9,11 +9,11 @@ import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const FontAwesomeMenu = (props) => {
-  const { icon, dest } = props
+  const { icon } = props
 
   return (
     <div>
-        <Link href={dest}>
+        <Link>
             <a>
                 <FontAwesomeIcon icon={icon} className="w-10 h-10 fill-current inline transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110" />
             </a>
