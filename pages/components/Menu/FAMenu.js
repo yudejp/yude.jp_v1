8a0897cc00fd5469f31a0a7091ad23c3b665c4ea
yudejp <i@yude.jp>
2021-08-09 11:58:29 +0900

    Update FAMenu.js

--- pages/components/Menu/FAMenu.js
+++ pages/components/Menu/FAMenu.js
@@ -9,7 +9,7 @@ const FontAwesomeMenu = (props) => {
 
   return (
     <div>
-        <Link href={dest}>
+        <Link href={`${dest}`}>
             <a>
                 <FontAwesomeIcon icon={icon} className="w-10 h-10 fill-current inline transition duration-200 ease-in-out transform hover:-translate-y-1 hover:scale-110" />
             </a>
