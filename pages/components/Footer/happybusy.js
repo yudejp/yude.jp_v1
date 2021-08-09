8f185e05dcdca8def57db460c47ed7d537493e18
yudejp <i@yude.jp>
2021-08-09 11:00:34 +0900

    Modularize happybusy

---
+++ pages/components/Footer/happybusy.js
@@ -0,0 +1,18 @@
+// Next.js
+import Link from 'next/link'
+
+const happybusy = (props) => {
+  return (
+    <Link href="https://sites.google.com/site/happybusy/">
+            <a>
+              <Image
+                src="/images/busy_banner.png"
+                width={200}
+                height={42}
+              />
+            </a>
+    </Link>
+  )
+}
+
+export default happybusy
