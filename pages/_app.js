94c966d58c3e7bf306a5ebe18b7284d4eb8c905b
yudejp <i@yude.jp>
2021-08-14 10:16:53 +0900

    Add comments, Load default SEO config

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
+// Tailwind CSS
 import 'tailwindcss/tailwind.css';
-import { ThemeProvider } from "next-themes";
 import "../styles/globals.css"
+
+// Dark-mode related
+import { ThemeProvider } from "next-themes";
+
+// next-seo
+import { DefaultSeo } from 'next-seo';
+import SEO from '../next-seo.config';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </ThemeProvider>
   );
