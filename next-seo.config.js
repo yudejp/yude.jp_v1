38022863275e53d18cbddc45d5864d30dbf26aaa
yudejp <i@yude.jp>
2021-08-14 10:11:50 +0900

    Add next-seo.config.js

---
+++ next-seo.config.js
@@ -0,0 +1,13 @@
+export default {
+    openGraph: {
+      type: 'website',
+      locale: 'ja_JP',
+      url: 'https://yude.jp/',
+      site_name: 'yude.jp',
+    },
+    twitter: {
+      handle: '@yudejp',
+      site: '@yudejp',
+      cardType: 'summary_large_image',
+    },
+  };
