4d0b82dee3f8279bc565e3f3b8748380b3706a12
yudejp <i@yude.jp>
2021-09-25 22:14:02 +0900

    Update [uuid].js

--- pages/api/PlayerName/[uuid].js
+++ pages/api/PlayerName/[uuid].js
@@ -12,7 +12,7 @@ const RawPlayerName = async (req, res) => {
     const response = await getName(uuid);
     const data = await response.json();
     if (response.status === 204 || response.status > 400) {
-      return res.status(200).send("404");
+      return res.status(200).send("読み込み中...");
     }
     const username = data.username;
     return res.status(200).json({
