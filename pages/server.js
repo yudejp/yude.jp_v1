b1a778b5d0eadbc1c47c97ce5061eef9449edd86
yudejp <i@yude.jp>
2021-10-13 09:45:36 +0900

    Update hostname

--- pages/server.js
+++ pages/server.js
@@ -32,7 +32,7 @@ export default function Server(props) {
           </div>
           
           {
-              // yui
+              // sandy
           }
           <h2 className="text-2xl text-left font-bold leading-7 sm:text-3xl sm:truncate">yui</h2>
           <div className="flex xl:w-2/3 text-center">
@@ -43,10 +43,24 @@ export default function Server(props) {
           </div>
           <iframe className="w-full h-96" src="https://mackerel.io/embed/public/embed/heHnGhDanoIDlf7jjxUe9yPVrsG3deeH5ptD8suErrq5w46crWEIYdLfzLoukzfF?period=24h" height="400" frameBorder="0" scrolling="no"></iframe>
           
+          
           {
-              // iroha
+              // oracle1
           }
-          <h2 className="text-2xl text-left font-bold leading-7 sm:text-3xl sm:truncate">iroha</h2>
+          <h2 className="text-2xl text-left font-bold leading-7 sm:text-3xl sm:truncate">oracle1</h2>
+          <div className="flex xl:w-2/3 text-center">
+          <div className="flex-1"><p className="font-bold">{t('service')} </p>Oracle Cloud</div>
+          <div className="flex-1"><p className="font-bold">{t('shape')} </p>VM.Standard.E2.1.Micro</div>
+          <div className="flex-1"><p className="font-bold">RAM </p>1GB</div>
+          <div className="flex-1"><p className="font-bold">OS </p>Ubuntu 20.04 LTS</div>
+          <div className="flex-1"><p className="font-bold">{t('region')} </p>Japan Central (Osaka)</div>
+          </div>
+          <iframe className="w-full h-96" src="https://mackerel.io/embed/public/embed/vcn6LE4P2hh6GzvtMbIAWxsXpDh6eSoNyAXdhfYaj9yDN6FJwOHIiz9ktqToj1HG?period=24h" height="400" frameBorder="0" scrolling="no"></iframe>
+          
+          {
+              // oracle2
+          }
+          <h2 className="text-2xl text-left font-bold leading-7 sm:text-3xl sm:truncate">oracle2</h2>
           <div className="flex xl:w-2/3 text-center">
           <div className="flex-1"><p className="font-bold">{t('service')} </p>Oracle Cloud</div>
           <div className="flex-1"><p className="font-bold">{t('shape')} </p>VM.Standard.E2.1.Micro</div>
@@ -56,20 +70,6 @@ export default function Server(props) {
           </div>
           <iframe className="w-full h-96" src="https://mackerel.io/embed/public/embed/wgmTz7aTgvwfyU6qVHrJJmQlpOHCUJK6qw0W9sJTX68WOI8TFtc27YvJ7dwkQEGv?period=24h" height="400" frameBorder="0" scrolling="no"></iframe>
           
-          
-          {
-              // komachi
-          }
-          <h2 className="text-2xl text-left font-bold leading-7 sm:text-3xl sm:truncate">komachi</h2>
-          <div className="flex xl:w-2/3 text-center">
-          <div className="flex-1"><p className="font-bold">{t('service')} </p>Oracle Cloud</div>
-          <div className="flex-1"><p className="font-bold">{t('shape')} </p>VM.Standard.E2.1.Micro</div>
-          <div className="flex-1"><p className="font-bold">RAM </p>1GB</div>
-          <div className="flex-1"><p className="font-bold">OS </p>Ubuntu 20.04 LTS</div>
-          <div className="flex-1"><p className="font-bold">{t('region')} </p>Japan Central (Osaka)</div>
-          </div>
-          <iframe className="w-full h-96" src="https://mackerel.io/embed/public/embed/vcn6LE4P2hh6GzvtMbIAWxsXpDh6eSoNyAXdhfYaj9yDN6FJwOHIiz9ktqToj1HG?period=24h" height="400" frameBorder="0" scrolling="no"></iframe>
-  
         </div>
       </Layout>
     </>
