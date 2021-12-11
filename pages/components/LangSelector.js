5a0e87d7d47bd439817b56c97e7f0803cf6c4ae4
yude <i@yude.jp>
2021-12-11 21:40:26 +0900

    Update icon size

--- pages/components/LangSelector.js
+++ pages/components/LangSelector.js
@@ -40,7 +40,7 @@ const Dropdown = ({ color }) => {
         : openDropdownPopover();
     }}
     >
-      <FontAwesomeIcon icon={faLanguage} className="w-12 h-9" />
+      <FontAwesomeIcon icon={faLanguage} className="text-4xl" />
       <svg className="-mr-1 ml-2 h-9 w-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
         <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
       </svg>
